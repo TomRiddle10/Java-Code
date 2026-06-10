c9f1332945965db04361d2e40ee2cd832db46388
Ziya Attar <230949675+TomRiddle10@users.noreply.github.com>
2026-06-10 14:07:12 +0530

    new files

--- Graph1.java
+++ Graph1.java
@@ -1,3 +1,88 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Queue;
+import java.util.LinkedList;
 public class Graph1 {
-    
+    static ArrayList<ArrayList<Integer>> gph=new ArrayList<>();
+    static void createGraph(int ver){
+        for(int i=0;i<ver;i++){
+            gph.add(new ArrayList<>());
+        }
+    }
+    static void addEdge(int src, int dest){
+        gph.get(src).add(dest);
+        gph.get(dest).add(src);
+    }
+    static void BFS(int src, int ver){
+        boolean[] visited=new boolean[ver];
+        Queue<Integer> q=new LinkedList<>();
+        q.add(src);
+        visited[src]=true;
+        while(!q.isEmpty()){
+            int data=q.poll();
+            System.out.print(data+" ");
+            for(int neighbour: gph.get(src)){
+                if(!visited[neighbour]){
+                    visited[neighbour]=true;
+                }
+            }
+        }
+    }
+    static void DFS(int src) {
+    Deque<Integer> st = new ArrayDeque<>();
+    boolean[] visited = new boolean[gph.size()];
+
+    st.push(src);
+    visited[src] = true;
+
+    while (!st.isEmpty()) {
+        int curr = st.peek();
+        System.out.print(curr + " ");
+
+        boolean found = false;
+
+        for (int next : gph.get(curr)) {
+            if (!visited[next]) {
+                st.push(next);
+                visited[next] = true;
+                found = true;
+                break; 
+            }
+        }
+
+        if (!found) {
+            st.pop(); 
+        }
+    }
+}
+    static boolean containsEdge(int src,int dest){
+        return gph.get(src).contains(dest);
+    }
+
+    static void deleteEdge(int src,int dest){
+        
+        gph.get(src).remove(Integer.valueOf(dest));
+        gph.get(dest).remove(Integer.valueOf(src));
+    }
+
+
+    public static void main(String[] args){
+        createGraph(6);
+        addEdge(0, 1);
+        addEdge(0,5);
+        addEdge(1,2);
+        addEdge(1,5);
+        addEdge(2,3);
+        addEdge(3,4);
+        addEdge(3,5);
+        System.out.println("");
+        BFS(0,6);
+        System.out.println("");
+        System.out.println("");
+        DFS(0);
+        System.out.println(containsEdge(0, 1));
+        deleteEdge(0, 5);
+        System.out.println(containsEdge(0, 5));
+    }
 }
--- TreeJ.java
+++ TreeJ.java
@@ -1,218 +1,268 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;    
-    class Node {
-            int data;
-            Node left;
-            Node right;
-            Node(int d) {
-                this.data = d;
-                this.left = null;
-                this.right = null;
-            }
-        }
-    class TreeOps {
-        Node buildTree(Integer[] arr) {
-    if (arr.length == 0 || arr[0] == null)
-        return null;
+import java.util.Queue;
+import java.util.ArrayList;
 
-    Queue<Node> que = new LinkedList<>();
-    Node root = new Node(arr[0]);
-    que.offer(root);
+class Node {
+    int data;
+    Node left;
+    Node right;
 
-    int i = 1; // start after root
-
-    while (!que.isEmpty() && i < arr.length) {
-        Node current = que.poll();
-
-        // Left child
-        if (i < arr.length && arr[i] != null) {
-            current.left = new Node(arr[i]);
-            que.offer(current.left);
-        }
-        i++;
-
-        // Right child
-        if (i < arr.length && arr[i] != null) {
-            current.right = new Node(arr[i]);
-            que.offer(current.right);
-        }
-        i++;
+    Node(int d) {
+        this.data = d;
+        this.left = null;
+        this.right = null;
     }
-
-    return root;
 }
-        Node createNode(int val){
-            Node newNode = new Node(val);
-            return newNode;
-        }
-        void preOrder(Node root){
-            if(root==null) return;
-            System.out.print(root.data+" ");
-            preOrder(root.left);
-            preOrder(root.right);
-        }
-        void inOrder(Node root){
-            if(root==null) return;
-            inOrder(root.left);
-            System.out.print(root.data+" ");
-            inOrder(root.right);
-        }
-        void postOrder(Node root){
-            if(root==null) return;
-            postOrder(root.left);
-            postOrder(root.right);
-            System.out.print(root.data+" ");
-        }
-        void levelOrder(Node root){
-            if(root==null) return;
-            Queue<Node> que=new LinkedList<>();
-            que.add(root);
-            while(!que.isEmpty()){
-                Node ele=que.poll();
-                System.out.print(ele.data+" ");
-                if(ele.left!=null)
-                    que.offer(ele.left);
-                if(ele.right!=null)
-                    que.offer(ele.right);
-            }
-        }
-        void preorderIterator(Node root){
-         Deque<Node> stack = new ArrayDeque<>();
-         stack.push(root);
 
-         while (!stack.isEmpty()) {
-             Node current = stack.pop();
-             System.out.print(current.data + " ");
-             if (current.right != null) {
-                 stack.push(current.right);
-             }
-             if (current.left != null) {
-                 stack.push(current.left);
-             }
-         }
-          System.out.println(stack);
+class TreeOps {
+    Node buildTree(Integer[] arr) {
+        if (arr.length == 0 || arr[0] == null)
+            return null;
 
-        }
-        boolean ismirror(Node root1, Node root2) {
-            if (root1 == null && root2 == null) {
-                return true;
-            }
-            if (root1 == null || root2 == null) {
-                return false;
-            }
-            if (root1.data!=root2.data) {
-                return false;
-                
-            }
+        Queue<Node> que = new LinkedList<>();
+        Node root = new Node(arr[0]);
+        que.offer(root);
 
-            
-            return (root1.data == root2.data) &&
-                    ismirror(root1.left, root2.right) &&
-                    ismirror(root1.right, root2.left);
+        int i = 1; // start after root
+
+        while (!que.isEmpty() && i < arr.length) {
+            Node current = que.poll();
+
+            // Left child
+            if (i < arr.length && arr[i] != null) {
+                current.left = new Node(arr[i]);
+                que.offer(current.left);
+            }
+            i++;
+
+            // Right child
+            if (i < arr.length && arr[i] != null) {
+                current.right = new Node(arr[i]);
+                que.offer(current.right);
+            }
+            i++;
         }
 
-        int height(Node root){
-            if(root==null) return 0;
-            return 1+Math.max(height(root.left),height(root.right));
-        }
-        int diameter=0;
-        int getDiameter(Node root){
-            if(root==null) return 0;
-            return 1+height(root.left)+height(root.right);
-        }
-        boolean isEqual(Node root1,Node root2){
-            if(root1==null&&root2==null) return true;
-            if(root1==null|| root2==null) return false;
-            if(root1.data!=root2.data) return false;
-            return root1.data==root2.data && isEqual(root1.left, root2.left) && isEqual(root1.right,root2.right);
-        }
-        void delete(Node root,int key){
-            if(root==null) return;
-            Queue<Node> que=new LinkedList<>();
-            Node keyNode=null;
-            Node curr=null;
-            que.add(root);
-            while(!que.isEmpty()){
-                curr=que.poll();
-                if(curr.data==key){
-                    keyNode=curr;
-                }
-                if(curr.left!=null) que.offer(curr.left);
-                if(curr.right!=null) que.offer(curr.right);
-            }
-            if(keyNode!=null){
-                keyNode.data=curr.data;
-                delLastNode(root, curr);
+        return root;
+    }
+
+    Node createNode(int val) {
+        Node newNode = new Node(val);
+        return newNode;
+    }
+
+    void preOrder(Node root) {
+        if (root == null)
+            return;
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    void inOrder(Node root) {
+        if (root == null)
+            return;
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    void postOrder(Node root) {
+        if (root == null)
+            return;
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    void levelOrder(Node root) {
+        if (root == null)
+            return;
+        Queue<Node> que = new LinkedList<>();
+        que.add(root);
+        while (!que.isEmpty()) {
+            Node ele = que.poll();
+            System.out.print(ele.data + " ");
+            if (ele.left != null)
+                que.offer(ele.left);
+            if (ele.right != null)
+                que.offer(ele.right);
         }
     }
-    void delLastNode(Node root,Node delNode){
-        Queue<Node> que=new LinkedList<>();
+
+    void preorderIterator(Node root) {
+        Deque<Node> stack = new ArrayDeque<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            Node current = stack.pop();
+            System.out.print(current.data + " ");
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+        }
+        System.out.println(stack);
+
+    }
+
+    boolean ismirror(Node root1, Node root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+        if (root1.data != root2.data) {
+            return false;
+
+        }
+
+        return (root1.data == root2.data) &&
+                ismirror(root1.left, root2.right) &&
+                ismirror(root1.right, root2.left);
+    }
+
+    int height(Node root) {
+        if (root == null)
+            return 0;
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+
+    int diameter = 0;
+
+    int getDiameter(Node root) {
+        if (root == null)
+            return 0;
+        return 1 + height(root.left) + height(root.right);
+    }
+
+    boolean isEqual(Node root1, Node root2) {
+        if (root1 == null && root2 == null)
+            return true;
+        if (root1 == null || root2 == null)
+            return false;
+        if (root1.data != root2.data)
+            return false;
+        return root1.data == root2.data && isEqual(root1.left, root2.left) && isEqual(root1.right, root2.right);
+    }
+
+    void delete(Node root, int key) {
+        if (root == null)
+            return;
+        Queue<Node> que = new LinkedList<>();
+        Node keyNode = null;
+        Node curr = null;
+        que.add(root);
+        while (!que.isEmpty()) {
+            curr = que.poll();
+            if (curr.data == key) {
+                keyNode = curr;
+            }
+            if (curr.left != null)
+                que.offer(curr.left);
+            if (curr.right != null)
+                que.offer(curr.right);
+        }
+        if (keyNode != null) {
+            keyNode.data = curr.data;
+            delLastNode(root, curr);
+        }
+    }
+
+    void delLastNode(Node root, Node delNode) {
+        Queue<Node> que = new LinkedList<>();
         que.offer(root);
-        while(!que.isEmpty()){
-            Node curr=que.poll();
-            if(curr.left==delNode){
-                curr.left=null;
+        while (!que.isEmpty()) {
+            Node curr = que.poll();
+            if (curr.left == delNode) {
+                curr.left = null;
                 return;
-            }else{
+            } else {
                 que.offer(curr.left);
             }
-            if(curr.right==delNode){
-                curr.right=null;
+            if (curr.right == delNode) {
+                curr.right = null;
                 return;
-            }else{
+            } else {
                 que.offer(curr.right);
             }
-            if(curr.left!=null) que.offer(curr.left);
-            if(curr.right!=null) que.offer(curr.right);
+            if (curr.left != null)
+                que.offer(curr.left);
+            if (curr.right != null)
+                que.offer(curr.right);
         }
     }
+    void dfs(Node root,int target,ArrayList<Integer> path,ArrayList<ArrayList<Integer>> res){
+        if(root==null) return;
+        path.add(root.data);
+        if(target-root.data==0){
+            res.add(new ArrayList<>(path));
+        }
+        dfs(root.left,target-root.data,path,res);
+        dfs(root.right,target-root.data,path,res);
+        path.remove(path.size()-1);
+    }
+
+    ArrayList<ArrayList<Integer>> findPath(Node root, int target) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return null;
+        ArrayList<Integer> path = new ArrayList<>();
+        dfs(root, target, path, res);
+        return res;
+    }
 }
-        
 
 public class TreeJ {
 
     public static void main(String[] args) {
 
         TreeOps tree = new TreeOps();
-        Node root = tree.createNode(10);
-        root.left=new Node(5);
-        root.right=new Node(15);
-        root.left.left=new Node(3);
-        root.left.right=new Node(7);
-        System.out.println("Pre order");
-        tree.preOrder(root);
-        System.out.println();
-        System.out.println("\nIn order");
-        tree.inOrder(root);
-        System.out.println();
-        System.out.println("\nPost order");
-        tree.postOrder(root);
-        System.out.println();
-        System.out.println("\nLevel order");
-        tree.levelOrder(root);
+
+        Node root = tree.buildTree(new Integer[]{5,4,8,11,13,4,7,2,1});
+        ArrayList<ArrayList<Integer>> res = tree.findPath(root, 22);
+        System.out.println(res);
+
+        // root.left = new Node(5);
+        // root.right = new Node(15);
+        // root.left.left = new Node(3);
+        // root.left.right = new Node(7);
+        // System.out.println("Pre order");
+        // tree.preOrder(root);
+        // System.out.println();
+        // System.out.println("\nIn order");
+        // tree.inOrder(root);
+        // System.out.println();
+        // System.out.println("\nPost order");
+        // tree.postOrder(root);
+        // System.out.println();
+        // System.out.println("\nLevel order");
+        // tree.levelOrder(root);
         // Node root = tree.buildTree(new Integer[]{10,20,30,40,50,60,70});
         // System.out.println("\nLevel order of built tree:");
         // tree.levelOrder(root);
         // System.out.println();
         // System.out.println("\nPre order of built tree:");
         // tree.preOrder(root);
-        Node root4=tree.buildTree(new Integer[]{10,20,20,30,40,40,30});
-        System.out.println("\nMirror check:");
-        System.out.println(tree.ismirror(root, root4));
-        System.out.println("\nHeight of the tree:");
-        System.out.println(tree.height(root));
-        System.out.println("\nDiameter of the tree:");
-        System.out.println(tree.getDiameter(root));
-        Node r1 = tree.buildTree(new Integer[]{10,20,30,40,50,60,70,null});
-        Node r2 = tree.buildTree(new Integer[]{10,20,30,40,50,60,70});
-        System.out.println("\nEquality check:");
-        System.out.println(tree.isEqual(r1, r2));
-        System.out.println("\nDelete node 15:");
-        tree.delete(root, 15);
-        tree.levelOrder(root);
+        // Node root4 = tree.buildTree(new Integer[] { 10, 20, 20, 30, 40, 40, 30 });
+        // System.out.println("\nMirror check:");
+        // System.out.println(tree.ismirror(root, root4));
+        // System.out.println("\nHeight of the tree:");
+        // System.out.println(tree.height(root));
+        // System.out.println("\nDiameter of the tree:");
+        // System.out.println(tree.getDiameter(root));
+        // Node r1 = tree.buildTree(new Integer[] { 10, 20, 30, 40, 50, 60, 70, null });
+        // Node r2 = tree.buildTree(new Integer[] { 10, 20, 30, 40, 50, 60, 70 });
+        // System.out.println("\nEquality check:");
+        // System.out.println(tree.isEqual(r1, r2));
+        // System.out.println("\nDelete node 15:");
+        // tree.delete(root, 15);
+        // tree.levelOrder(root);
+
     }
 }
-
-
